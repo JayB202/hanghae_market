32f2ce20253ddcc4f02d3b259eafb719f8734d22
Shuan75 <limbboy@nate.com>
2023-05-13 19:08:52 +0900

    Feat : signup, login, logout commit

--- src/main/java/com/example/hanghae_market/entity/User.java
+++ src/main/java/com/example/hanghae_market/entity/User.java
@@ -1,14 +1,42 @@
 package com.example.hanghae_market.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
 
-@Entity
+@NoArgsConstructor
+@Getter
+@Entity(name = "TB_USER")
 public class User {
-
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "userId", nullable = false)
     private String userId;
+
+    @Column(nullable = false)
+    private String password;
+
+//    @Column(nullable = false)
+//    private String username;
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String location;
+
+    @Column(nullable = false)
+    private String phone;
+
+    @Column(nullable = false)
+    @Enumerated(value = EnumType.STRING)
+    private UserRole role;
+
+    public User(String userId, String password, String email, String location, String phone, UserRole role) {
+        this.userId = userId;
+        this.password = password;
+        this.email = email;
+        this.location = location;
+        this.phone = phone;
+        this.role = role;
+    }
 }
