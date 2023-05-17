055dcd4cfda2570b7044c79e8597dc5f0c4c1d0e
youngkikim14 <127404498+youngkikim14@users.noreply.github.com>
2023-05-17 23:28:31 +0900

    Update PostResponseDto.java

--- src/main/java/com/example/hanghae_market/dto/PostResponseDto.java
+++ src/main/java/com/example/hanghae_market/dto/PostResponseDto.java
@@ -1,5 +1,6 @@
 package com.example.hanghae_market.dto;
 
+import com.example.hanghae_market.entity.ImagePath;
 import com.example.hanghae_market.entity.Interest;
 import com.example.hanghae_market.entity.Post;
 import lombok.AllArgsConstructor;
@@ -7,13 +8,14 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
 public class PostResponseDto {
 
-    //    private List<ImagePath> imagePathList;
+    private List<ImagePathResponseDto> imagePathList;
     private Long postId;
     private String postTitle;
     private String postContent;
@@ -26,9 +28,14 @@ public class PostResponseDto {
     private LocalDateTime modifiedAt;
     private String specificLocation;
     private Boolean isShared;
+    private Boolean myinterest;
 
     public PostResponseDto(Post post) {
-//        this.imagePathList = post.getImagePathList();
+//         for (ImagePath imagePath: post.getImagePathList()) {
+//             ImagePathResponseDto imagePathResponseDto = new ImagePathResponseDto(imagePath);
+//             this.imagePathList.add(imagePathResponseDto);
+//         }
+
         this.postId = post.getPostId();
         this.postTitle = post.getPostTitle();
         this.postContent = post.getPostContent();
@@ -41,5 +48,7 @@ public class PostResponseDto {
         this.modifiedAt = post.getModifiedAt();
         this.specificLocation = post.getSpecificLocation();
         this.isShared = post.getIsShared();
+        this.myinterest = post.getInterests().stream()
+            .anyMatch(interest -> interest.getUser().getUserId().equals(userId) && interest.getInterest_status());
     }
 }
